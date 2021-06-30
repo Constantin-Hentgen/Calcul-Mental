bd838378d474a1c0ac8e216a6af994a5b00c26ef
Constantin-Hentgen <constantin.hentgen@protonmail.com>
2021-06-30 15:53:51 +0200

    on my way

--- math.php
+++ math.php
@@ -11,9 +11,9 @@
             //$_SESSION['nombre_A'] = random_int(1,10);
             //$_SESSION['nombre_B'] = random_int(1,10);
             //echo "<strong>",$_SESSION['nombre_A'],"+",$_SESSION['nombre_B'],"</strong>";
-            echo "<strong>",$_SESSION['nombre_A'],$_SESSION['operation'],$_SESSION['nombre_B'],"</strong>";
+            echo "<strong id='strong'>",$_SESSION['nombre_A'],$_SESSION['operation'],$_SESSION['nombre_B'],"</strong>";
         ?>
-        <form action="<?=$_SERVER['PHP_SELF']?>" method="post">
+        <form id='form' action="<?=$_SERVER['PHP_SELF']?>" method="post">
             <input type="variable" name="variable" autofocus autocomplete="off"/>
         </form>
         <?php
@@ -21,16 +21,21 @@
             if (isset($_POST['variable'])){
                 if ((int)$_POST['variable']===$result)
                 {
-                    echo "victory";                    
+                    echo "
+                    <script>
+                        document.getElementById('form').style.display = 'none';
+                        document.getElementById('strong').innerHTML = 'Well done';
+                        document.body.style.background = 'green';
+                        document.form.style.display = 'none';
+                    </script>";                    
                     $_SESSION['nombre_A'] = random_int(1,10);
                     $_SESSION['nombre_B'] = random_int(1,10);
-                    header("math.php");
+                    header("Refresh:2");
                 }
                 else
                 {
                     header("math.php");
                     echo "<small>Wrong input…</small>";
-                    echo '<small>entrée : ',$_POST['variable'],' ; solution : ',$result,'</small>';
                 }
             }
             //gestion générique en cas de problème
